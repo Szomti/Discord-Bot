b3ccc0896d5705357b6af7d59e49ca23b056256f
Szomti <piotrpiotr1285@gmail.com>
2024-06-02 15:25:51 +0200

    Fixed commands

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import discord
-from ids import * 
+from datetime import datetime
+from constants import * 
 from discord.ext import commands
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix='/', intents=intents)
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 def role_for_channel(channel: int, guild: discord.Guild) -> discord.Role:
     match channel:
@@ -22,29 +23,24 @@ def role_for_channel(channel: int, guild: discord.Guild) -> discord.Role:
         case _:
             return None
         
-@bot.command()
-@discord.app_commands.checks.has_permissions(administrator=True)
+@bot.hybrid_command()
 async def hello(ctx):
     await ctx.send('Thanks for check-in', ephemeral=True)
 
-@bot.command()
-@discord.app_commands.checks.has_permissions(administrator=True)
+@bot.hybrid_command()
 async def roles(ctx):
     await ctx.send(f"React to get role:\n:infinity: - {ctx.guild.get_role(RolesIds.everything).mention}\n:one: - {ctx.guild.get_role(RolesIds.neu).mention}\n:two: - {ctx.guild.get_role(RolesIds.skytils).mention}\n:three: - {ctx.guild.get_role(RolesIds.skyhanni).mention}\n:four: - {ctx.guild.get_role(RolesIds.bazaar_notifier).mention}\n:five: - {ctx.guild.get_role(RolesIds.skyblock_addons).mention}")
 
     
 @bot.event
 async def on_ready():
-    print("Bot is Ready")
-    try:
-        synced = await bot.tree.sync()
-        print(f"Synced {len(synced)} command(s)")
-    except Exception as e:
-        print(e)
+    await bot.tree.sync()
+    print(">> Bot is Ready <<\n[" + str(datetime.now()) + "]")
 
 @bot.event
 async def on_message(message):
     if message.author != bot.user:
+        await bot.process_commands(message)
         guild = message.guild
         if(guild.id != GUILD_ID):
             return await message.channel.send("Server ID not correct")
