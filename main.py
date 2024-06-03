78f9bbe21ef17814af138359fb4514c9d5d964d1
Szomti <piotrpiotr1285@gmail.com>
2024-06-04 00:15:38 +0200

    Added embeds; Added welcome_info command

--- main.py
+++ main.py
@@ -21,11 +21,29 @@ bot = commands.Bot(command_prefix='?', intents=intents)
 @bot.command(name='debug', description='debug stuff etc.')
 @commands.has_role(Roles.admin.id)
 async def debug(ctx: discord.ApplicationContext, data: int | None):
-    try:
-        guild = bot.get_guild(GUILD_ID)
-        await ctx.send(guild.get_member(data))
-    except Exception as e:
-        await ctx.send(e)
+    description = 'It\'s basically server for me to have every mod update in one place and get notified about it.\n'
+    description += '\nJust a heads-up, the bot is running on my pc, so it won\'t be always online'
+    description += '\n*(when it comes back online, it will ping the respective role if there were updates in the meantime)*'
+    embed = discord.Embed(
+        title = 'Welcome to Skyblock Mods server!',
+        description = description,
+        color = discord.Colour.blue(),
+    )
+    await ctx.send(embed=embed)
+
+@bot.command(name='welcome_info', description='Create embed with basic server info')
+@commands.has_role(Roles.admin.id)
+async def welcome_info(ctx: discord.ApplicationContext, data: int | None):
+    description = 'It\'s basically server for me to have every hypixel skyblock mod update in one place and get notified about it.\n'
+    description += '\nJust a heads-up, the bot is running on my pc, so it won\'t be always online'
+    description += '\n*(when it comes back online, it will ping the respective role if there were updates in the meantime)*'
+    embed = discord.Embed(
+        title = 'Welcome to Skyblock Mods server!',
+        description = description,
+        color = discord.Colour.blue(),
+    )
+    embed.add_field(name='Rules', value='Don\'t break [discord\'s ToS](https://discord.com/terms/)')
+    await ctx.send(embed=embed)
 
 @bot.command(name='clear', description='clear messages')
 @commands.has_role(Roles.admin.id)
@@ -45,12 +63,22 @@ async def roles(ctx: discord.ApplicationContext):
         if roles_checkpoint.channel_id is not None and roles_checkpoint.message_id is not None:
             channel = bot.get_channel(roles_checkpoint.channel_id)
             if channel is not None:
-                await channel.get_partial_message(roles_checkpoint.message_id).delete()
-        info = 'React to get role:'
+                try:
+                    await channel.get_partial_message(roles_checkpoint.message_id).delete()
+                except:
+                    pass
+        description='React to get role!'
+        embed = discord.Embed(
+            title = 'Want to be notified about mod updates?',
+            description = description,
+            color = discord.Colour.blue(),
+        )
+        info =''
         for role in Roles.full_list:
             if role.icon is not None:
                 info += f'\n{role.icon} - {ctx.guild.get_role(role.id).mention}'
-        sent = await ctx.send(info)
+        embed.add_field(name='Available roles:', value=info)
+        sent = await ctx.send(embed=embed)
         roles_checkpoint.message_id = sent.id
         roles_checkpoint.channel_id = sent.channel.id
         roles_checkpoint.save_data()
@@ -62,7 +90,12 @@ async def roles(ctx: discord.ApplicationContext):
 
 @bot.slash_command(description='Register app comand usage (badge)')
 async def hello(ctx: discord.ApplicationContext):
-    await ctx.respond('Thanks for check-in')
+    embed = discord.Embed(
+        title='Hello fellow member!',
+        description='Thanks for checking in and for using this command. Usage of this command allows me to have developer badge!',
+        color=discord.Colour.blue(),
+    )
+    await ctx.respond(embed=embed)
     
 @bot.listen(once=True)
 async def on_ready():
@@ -76,12 +109,14 @@ async def on_ready():
     log('Saving checkpoint')
     updates_checkpoint.save_data()
     log('Finished saving')
-    log(f">> Bot is Ready <<")
-    await guild.get_channel(ChannelId.bot_info).send(f'<t:{int(datetime.now().timestamp())}> Bot Started')
+    await moderate_on_ready()
+    log(f'>> Bot is Ready <<')
+    date = datetime.now()
+    await guild.get_channel(ChannelId.bot_info).send(f'<t:{int(date.timestamp())}:d><t:{int(date.timestamp())}:T> Bot Started')
 
 @bot.listen()
 async def on_member_join(member: discord.Member):
-    member.add_roles(member.guild.get_role(Roles.member.id))
+    await member.add_roles(member.guild.get_role(Roles.member.id))
 
 @bot.listen()
 async def on_message(message: discord.Message):
@@ -89,9 +124,19 @@ async def on_message(message: discord.Message):
         guild = message.guild
         channel = message.channel
         if(guild.id != GUILD_ID):
-            return await channel.send("Guild ID not correct")
+            return await channel.send('Guild ID not correct')
+        if message.channel.id == ChannelId.hello:
+            await message.delete()
+            return
         if await ping_by_channel(guild, channel):
             updates_checkpoint.save_data()
+
+async def moderate_on_ready():
+    guild = bot.get_guild(GUILD_ID)
+    channel = guild.get_channel(ChannelId.hello)
+    async for message in channel.history(limit=200):
+        if message.author != bot.user:
+            await message.delete()
 
 @bot.listen()
 async def on_raw_reaction_add(payload: discord.RawReactionActionEvent):
@@ -115,7 +160,7 @@ async def ping_by_channel(guild: discord.Guild, channel: discord.TextChannel) ->
     role = guild.get_role(Channel(channel.id).get_role_id())
     if role == None: return False
     all_role = guild.get_role(Roles.all)
-    await channel.send("Update: "+all_role.mention+" "+role.mention)
+    await channel.send('Update: '+all_role.mention+' '+role.mention)
     return True
 
 async def check_new_data_by_channel(guild: discord.Guild, channel_id: int):
@@ -127,4 +172,4 @@ async def check_new_data_by_channel(guild: discord.Guild, channel_id: int):
         await ping_by_channel(guild, temp_channel)
         updates_checkpoint.set_data_by_channel(channel_id, temp_channel.last_message_id)
         
-bot.run(os.getenv("TOKEN"))
+bot.run(os.getenv('TOKEN'))
